0cd215690c81880c37c725b9e011881f71885c9c
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-09-11 23:18:24 +0200

    fix: theme background

--- app/src/main/java/com/github/lasnelus/Capybara/MainActivity.kt
+++ app/src/main/java/com/github/lasnelus/Capybara/MainActivity.kt
@@ -9,9 +9,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,44 +27,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CapybaraTheme {
-                Column(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = stringResource(id= R.string.home_today),
-                        color = MaterialTheme.colorScheme.secondary,
-                        style = MaterialTheme.typography.titleSmall,
-                        textAlign = TextAlign.Center
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.il_sun_cloud),
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxWidth(fraction = 0.85f)
-                    )
-                    Text(text = stringResource(id= R.string.degree_value),
-                        color = MaterialTheme.colorScheme.secondary,
-                        style = MaterialTheme.typography.titleSmall,
-                        textAlign = TextAlign.Center
-                    )
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PreviewHomeScreen() {
+    CapybaraTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            HomeScreen()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CapybaraTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+        Text(
+            text = stringResource(R.string.home_today),
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center
+        )
+        Image(
+            painter = painterResource(id = R.drawable.il_sun_cloud),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(0.85f)
+        )
+        Text(
+            text = stringResource(R.string.degree_value, 30),
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center
+        )
     }
 }
